15212c0fb6c1be59109014862787c0a33320d3e8
mudamuda <102901724+AldiAgung@users.noreply.github.com>
2024-06-19 20:00:47 +0700

    baru

--- tampilan.py
+++ tampilan.py
@@ -3,9 +3,13 @@ import pickle
 import pandas as panda
 import numpy as np
 
-tampilan = Blueprint(__name__, "tampilan")
-model = pickle.load(open(' ', 'rb'))
+# tampilan = Blueprint(__name__, "tampilan")
+# model = pickle.load(open(' ', 'rb'))
 
-@tampilan.route("/<name>")
-def beranda():
-    return render_template("interface.html")
+# @tampilan.route("/tampilan", methods = ['POST'])
+# def prediksi():
+#     return render_template('interface.html')
+
+# @tampilan.route("/<name>")
+# def beranda():
+#     return render_template("interface.html")
